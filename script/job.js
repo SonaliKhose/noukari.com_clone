7235203fab7ee79e40badd62c62f8bdae01fe8f3
SonaliKhose <sonalikhose600@gmail.com>
2023-04-02 19:14:14 +0530

    Done with day-2

---
+++ script/job.js
@@ -0,0 +1,38 @@
+let flag = 0;
+function controller(x){
+    flag = flag + x;
+    slidShow(flag);
+}
+slidShow(flag)
+function slidShow(num){
+    let slides = document.getElementsByClassName("slid");
+
+    if(num == slides.length){
+        flag = 0;
+        num = 0;
+    }
+    if(num<0){
+        flag = slides.length-1;
+        num = slides.length-1;
+    }
+    for(let y of slides){
+        y.style.display = "none";
+    }
+
+    slides[num].style.display = "block";
+}
+
+
+
+let profileArray=JSON.parse(localStorage.getItem("details"))
+
+
+
+
+let out=document.querySelector("#logOut")
+out.addEventListener("click",()=>{
+  
+  window.localStorage.removeItem('logdetails');
+  
+  window.location.href="index.html"
+})
